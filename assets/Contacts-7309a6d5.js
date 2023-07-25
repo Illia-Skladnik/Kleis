0ca475039e0753831b71c6697b8b65e2578ac675
Ivan <ivansh775@gmail.com>
2023-07-25 13:47:28 +0300

    dev

--- assets/Contacts-7309a6d5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,n as t,y as _}from"./index-170eb8ae.js";const c={};function n(a,o){return t(),_("div",null," Contacts ")}const r=e(c,[["render",n],["__scopeId","data-v-74eda010"]]);export{r as default};
